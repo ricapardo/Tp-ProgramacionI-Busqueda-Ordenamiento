ad56d2717b49e135d778584fabaca13c3dd57443
ricapardo <ricapardo84@gmail.com>
2025-06-02 20:09:16 -0300

    definicion de bubble sort y bloque main

---
+++ tp_integrador_I.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+
+if __name__ == "__main__":
+    datos = [34, 7, 23, 32, 5, 62]
+    print("Lista original:      ", datos)
+
+    # Ordenamiento
+    bubble_sort(datos)
+    print("Lista ordenada:      ", datos)
